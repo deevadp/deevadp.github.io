6e607639a67ce06e73863b2abe248669c93d80ae
deevadp <deevadaniswara@gmail.com>
2026-05-07 20:49:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -123,8 +123,7 @@ const projects = {
     Apparel: {
       title: "Apparel & Merchandise",
       tags: "Branding • Apparel Graphics",
-      desc: "Garment graphics and apparel-focused branding applications exploring merchandise, typography, and wearable visual identity systems.
-",
+     desc: "Garment graphics and apparel-focused branding applications exploring merchandise, typography, and wearable visual identity systems.",
       media: [
         { type: "image", src: "assets/The_Daffodil_Merch.png", alt: "The Daffodil Merch" } ,
         { type: "image", src: "assets/runforthewild_merch.png", alt: "Run For The Wild Merch" },
